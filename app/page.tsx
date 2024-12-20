7315ece7e885db3691822d5a78b0caef76064aae
Sachin Sheoran <sachinsheoran@Sachins-MacBook-Air.local>
2024-12-20 11:41:19 +0100

    impr: work info & card

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,66 @@
 import Link from "next/link";
 
-const jobObject: {
+const workHistoryInfoList: Array<{
 	company: string;
 	url: string;
 	role: string;
 	time: string;
 	description: string[];
-} = {
-	company: "Yellow AI",
-	url: "https://yellow.ai",
-	role: "Software Development Engineer",
-	time: "June 2022 - July 2024",
-	description: [
-		"Developed and maintained the backend of the Yellow AI platform.",
-		"Optimized the database for better performance.",
-		"Built a recommendation system for the platform.",
-		"Worked on the frontend of the platform.",
-	],
-};
+	skills: string[];
+}> = [
+	{
+		company: "Yellow AI",
+		url: "https://yellow.ai",
+		role: "Software Development Engineer",
+		time: "June 2022 - July 2024",
+		description: [
+			"Implemented an authentication sequence in the bot platform to prompt identity verification for unrecognized users prior to proceeding.",
+			"Engineered a new node to create an inventory update pipeline for customer bot tables, enabling real-time product displays for end users and successfully tested with up to 200,000 products per bot.",
+			"Developed a testing module for bot validation, allowing users to input utterances and FAQs to verify correct bot flow activation, significantly decreasing bot testing time.",
+			"Refactored the event hub and implemented an event processing pipeline with RabbitMQ. Integrated visualization and alerts using Prometheus Query Language.",
+			"Added version history for the prompts in various nodes. Enhanced internal logging and provided bot developers with access through S3 with up to one-hour precision.",
+			"Resolved multiple bugs and optimized various nodes while on on-call duty.",
+		],
+		skills: [
+			"Node.js",
+			"Express",
+			"MongoDB",
+			"RabbitMQ",
+			"Elasticsearch",
+			"Docker",
+			"Kubernetes",
+			"Typescript",
+			"Javascript",
+			"Agile",
+			"Postgres",
+			"Redis",
+		],
+	},
+	{
+		company: "Park+",
+		url: "https://parkplus.io",
+		role: "Software Development Engineer",
+		time: "Sept 2021 - June 2022",
+		description: [
+			"Refactored a Django-based Parking Management System to Golang, overseeing the full development lifecycle and maintaining sole codebase ownership.",
+			"Enhanced vehicle ingress/egress efficiency via RFID and Fastag integration, achieving sub-1-second processing.",
+			"Engineered a flexible system architecture for site-specific customization, including Fastag billing options and access control by vehicle type.",
+			"Created an API module to enable parking management via POS devices.",
+		],
+		skills: ["Golang", "Redis", "Docker", "MySQL", "MongoDB", "MQTT"],
+	},
+	{
+		company: "Marktine",
+		url: "https://marktine.com",
+		role: "Software Development Engineer Intern",
+		time: "May 2020 - Aug 2020",
+		description: [
+			"Worked on React Js and with the team, transformed a web-app into a fully fledged react-app.",
+			"Create core components and pages for the app.",
+		],
+		skills: ["React", "Javascript", "HTML", "CSS", "Redux"],
+	},
+];
 
 export default function Home() {
 	return (
@@ -56,30 +99,46 @@ export default function Home() {
 						<div className="debug col-span-1">
 							<div className="w-1 rounded-md bg-foreground-200 h-full md:left-3/4 relative"></div>
 						</div>
-						<div className="debug col-span-11 grid grid-flow-row">
-							<div className="debug">
-								<p>
-									<span className="text-left w-2/4 inline-block text-3xl">
-										<a href={jobObject.url}>{jobObject.company}</a>
-									</span>
-									<span className="text-right w-2/4 inline-block text-xl">
-										{" "}
-										{jobObject.time}
-									</span>
-									<br />
-									<span className="inline-block font-light text-lg">
-										{" "}
-										{jobObject.role}
-									</span>
-									<ul className="list-disc p-2">
-										{jobObject.description.map((desc, index) => (
-											<li key={index}>{desc}</li>
-										))}
-									</ul>
-								</p>
-							</div>
-							<div className="debug text-8xl">two</div>
-							<div className="debug text-8xl">three</div>
+						<div className="debug col-span-11 grid grid-flow-row space-y-5">
+							{workHistoryInfoList.map((jobObject, index) => (
+								<>
+									<div
+										className="debug box-border shadow-md shadow-background-400 hover:shadow-background-600 rounded-md p-4"
+										key={index}
+									>
+										<p>
+											<span className="text-left w-2/4 inline-block text-3xl">
+												<a href={jobObject.url}>{jobObject.company}</a>
+												<hr className="w-1/2 bg-foreground-200 h-1" />
+											</span>
+											<span className="text-right w-2/4 inline-block text-xl">
+												{" "}
+												{jobObject.time}
+											</span>
+											<br />
+											<span className="inline-block font-light text-lg">
+												{" "}
+												{jobObject.role}
+											</span>
+										</p>
+										<ul className="list-disc p-2">
+											{jobObject.description.map((desc, index) => (
+												<li key={index}>{desc}</li>
+											))}
+										</ul>
+										<div className="flex flex-wrap">
+											{jobObject.skills.map((skill, index) => (
+												<div
+													key={index}
+													className="m-1 p-2 bg-background-300 text-foreground-200 rounded-lg font-mono"
+												>
+													{skill}
+												</div>
+											))}
+										</div>
+									</div>
+								</>
+							))}
 						</div>
 					</div>
 				</div>
