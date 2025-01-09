1e4c0824f742cf635c9b16f5c35c2492df4b8c01
Sachin Sheoran <sachinsheoran@Sachins-MacBook-Air.local>
2025-01-09 17:54:54 +0100

    feat: add blog for operating system

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,3 +1,26 @@
-export default function Page() {
-    return <h1>This is the blogs page. Ideally it should have a list of blogs!</h1>
+import Link from "next/link";
+
+const blogPosts = [
+	{
+		title: "Operating System",
+		slug: "operating-system",
+		description: "Some content about operating systems",
+		tags: ["operating-system", "os"],
+		image: "https://images.unsplash.com/photo-1622838320004-4b3b3b3b3b3b",
+	},
+];
+
+export default function BlogsPage() {
+	return (
+		<div>
+			<h1>Blogs</h1>
+			<ul>
+				{blogPosts.map((post) => (
+					<li key={post.slug}>
+						<Link href={`/blogs/${post.slug}`}>{post.title}</Link>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
 }
